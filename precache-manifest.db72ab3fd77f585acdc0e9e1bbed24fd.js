6b53c1fe610be4fda9ab75426668935d057620f3
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-08-02 09:36:08 +0530

    Updates

---
+++ precache-manifest.db72ab3fd77f585acdc0e9e1bbed24fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1968c023694df4e65904cdec05fb69f0",
+    "url": "/youtube-react/index.html"
+  },
+  {
+    "revision": "520fc4fa675cf0263a50",
+    "url": "/youtube-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/youtube-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "520fc4fa675cf0263a50",
+    "url": "/youtube-react/static/js/main.53c4e40b.chunk.js"
+  },
+  {
+    "revision": "fe24961fa718673f7952",
+    "url": "/youtube-react/static/js/runtime~main.0fe0a208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
